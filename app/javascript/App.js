f2a9a8fd602aac59f46061ae29feaf3818b122ba
Goruchie <marcosgualtero43253@gmail.com>
2023-03-02 22:22:27 -0300

    Implement react-router and route

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Greeting from './pages/Greetings';
 
-function App() {
+function App () {
     return (
-        <Greeting/>
+      <BrowserRouter>
+        <Routes>
+           <Route exact path="/" element={<Greeting />} />
+        </Routes>
+      </BrowserRouter>
     );
 }
 
